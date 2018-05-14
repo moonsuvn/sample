3ce443764905f1dff1cd870f9b2d7c218b036e9c
moonsuvn <moonsuvn2@gmail.com>
2018-05-14 15:13:30 +0000

    Add users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     {
     	Model::unguard();
 
-        //$this->call(UsersTableSeeder::class);
-		$this->call(BikesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+		//$this->call(BikesTableSeeder::class);
 
         Model::reguard();
     }
