b399e69ebe8470434123e6a06d35a9ee85e1f34b
moonsuvn <moonsuvn2@gmail.com>
2018-05-29 02:47:45 +0000

    test22

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,7 +1,7 @@
 <header class="navbar navbar-fixed-top navbar-inverse" role="navigation">
-  <div class="container">
+  <div class="container" style="padding-right: 0px">
     <div class="col-md-offset-1 col-md-10 col-xs-10">
-      <div class="navbar-header" style="padding-right: 20px">
+      <div class="navbar-header" >
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
         <span class="sr-only">切换导航</span>
         <span class="icon-bar"></span>
